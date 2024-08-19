6192a9f956e6f9d33dee949f122a4eaefbec8a9e
TheRealPad <pierre-alexandre.delgado-arevalo@epitech.eu>
2024-08-19 21:47:07 +0200

    [MOD] update group members

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,53 +3,33 @@ import { Team } from "@/types/blog";
 const blogData: Team[] = [
   {
     id: 1,
-    name: "Bachir Benzaoui",
-    role: "Cybersecurity engineer"
-  },// generae more data 10 times like the previous one
+    name: "Pierre-Alexandre DELGADO-AREVALO",
+    role: "Lead Developer Front-end - chef de projet"
+  },
   {
     id: 2,
-    name: "Pierre-Alexandre DELGADO-AREVALO",
-    role: "Developer Fullstack"
-  },
-  {
-    id: 3,
-    name: "Vincent ZHONG",
-    role: "Developer Fullstack"
-  },
-  {
-    id: 4,
     name: "Wassini BOUZIDI",
     role: "Lead developer back-end"
   },
   {
-    id: 5,
+    id: 3,
     name: "Joël JOKA",
     role: "Front-end developer"
   },
   {
-    id: 6,
+    id: 4,
     name: "Tao WEIJIE",
     role: "Back-end developer"
   },
   {
-    id: 7,
-    name: "Sandro SGRO",
-    role: "Front-end developer"
-  },
-  {
-    id: 8,
+    id: 5,
     name: "Brice BOUALAVONG",
     role: "DevOps engineer"
   },
   {
-    id: 9,
+    id: 6,
     name: "Luc SCHMITT",
     role: "Front-end developer"
   },
-  {
-    id: 10,
-    name: "Titouan LEFÈVRE",
-    role: "Front-end developer"
-  }
 ];
 export default blogData;
